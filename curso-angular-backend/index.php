8128aedfe3529677f718e43ad8d502564bb62475
LauraCancelaCastro <cancelacastrolaura@gmail.com>
2021-12-20 19:31:23 +0100

    Lecciones 01-63

---
+++ curso-angular-backend/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get("/pruebas", function() use($app){
+    echo "Hola mundo desde Slim PHP";
+});
+
+$app->get("/probando", function() use($app){
+    echo "OTRO TEXTO CUALQUIERA";
+});
+$app->run();
+?>
